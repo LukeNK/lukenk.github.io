cabf81e07e35f6dc3ff0def9007fae026236fb48
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-04-28 08:36:03 +0700

    [Auto-commit] v24.021

--- asset/script/browser.js
+++ asset/script/browser.js
@@ -1,17 +1,48 @@
-// TODO: find out why this did not work
-// somehow the click event wasn't assigned to the element despite added event
-// document.querySelectorAll('.browser-filter > button').forEach((elm, key) => {
-//     elm.addEventListener('click', () => {
-//         projectFilter(key)
-//     });
-// });
-
-
-function browserFilter(key) {
+/**
+ * Call when a button frol the browser filter being clicked
+ * @param {HTMLButtonElement} btn The button that was clicked
+ */
+function browserFilter(btn) {
+    let cat = ''; // category, '' for all category filter
     // change the color of the button
     document.querySelectorAll('.browser-filter > button')
     .forEach((elm, l1) => {
-        if (key == l1) elm.classList.add('selected');
-        else elm.classList.remove('selected');
+        if (elm == btn) {
+            elm.classList.add('selected');
+            if (l1 != 0) cat = elm.innerHTML;
+        } else
+            elm.classList.remove('selected');
     });
+
+    // if the post have the category name
+    document.querySelectorAll('.browser-list > a').forEach(post => {
+        if (
+            post.innerHTML.toLowerCase()
+            .includes(cat.toLowerCase())
+        )
+            post.style.display = '';
+        else
+            post.style.display = 'none';
+    });
+
+    // save category filter when navigate between browsers through URL query
+    // document.querySelectorAll('.browser-nav > a').forEach(page => {
+    //     let link = page.getAttribute('href');
+    //     link += '?filter=' + cat.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    //     page.setAttribute('href', link);
+    // })
 }
+
+// check window location for filter property
+// {
+//     let link = new URL(window.location.href);
+//     for (let btn of document.querySelectorAll('.browser-filter > button'))
+//         if (
+//             link.searchParams.get('filter')
+//             == btn.innerHTML.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+//         ) {
+//             console.log(btn.innerHTML.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
+//             btn.click();
+//             break
+//         }
+// }
