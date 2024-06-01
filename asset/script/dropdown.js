ea35f802ac207bca076f26fc7f4835f7d6f58f76
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-06-01 21:12:09 +0700

    [Auto-commit] v24.027

---
+++ asset/script/dropdown.js
@@ -0,0 +1,24 @@
+// add to the end of the document
+document.querySelectorAll('.dropdown-click')
+.forEach(dropdown => {
+    dropdown.querySelectorAll('summary')
+    .forEach(sum => {
+        sum.onclick = () =>
+            dropdown.querySelectorAll('details')
+                .forEach(elm =>elm.removeAttribute('open'))
+
+        // add button for desktop
+        let button = document.createElement('button');
+        button.innerHTML = sum.innerHTML;
+
+        button.onclick = () => {
+            dropdown.querySelectorAll('button')
+                .forEach(btn => btn.removeAttribute('open'));
+            button.setAttribute('open', true);
+            sum.click();
+        }
+
+        dropdown.querySelector('.desktop').append(button);
+    });
+    dropdown.querySelector('.desktop button').click(); // open the first element
+})
