9abe7c3208efffc5d1b755865b982dc519dde903
Luke Nguyen Khanh <64694123+LukeNK@users.noreply.github.com>
2024-07-18 16:07:56 +0700

    Clean up repo

--- asset/script.js
+++
@@ -1,8 +0,0 @@
-// auto-close details
-document.querySelectorAll('.close-details').forEach(group => {
-    let allDetails = group.querySelectorAll('details');
-    allDetails.forEach(details =>
-        details.querySelector('summary').onclick = () =>
-            allDetails.forEach(elm => elm.removeAttribute('open'))
-    )
-})
