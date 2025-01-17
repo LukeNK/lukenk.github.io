6fe3a47a345dca4933f272abf3192f495a4264f2
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2025-01-17 21:45:23 +0700

    Quote auto-sort in console

--- quotes.js
+++ quotes.js
@@ -1,126 +1,350 @@
 // WARNING: other projects also depends on this file for quotes
 // T24
 
-const Quotes = [
-    ["", "Be kind, I don't care why"],
-    ["", "Be the change you want to see in the world"],
-    ["", "C'est juste un arrêt, pas le terminus."],
-    ["", "Devil works hard for his paycheck but you work harder."],
-    ["", "Don't lose sight of the moment that you're given"],
-    ["", "Event a broken clock is right twice a day"],
-    ["", "Even if I am a free wind, I'll cherish those who love me"],
-    ["", "Everybody is an astronaut because the Earth, after all, is in space"],
-    ["", "I might not be normal, but I am happy"],
-    ["", "I will be the one who light the flame"],
-    ["", "The horror persists, but so do I"],
-    ["", "The world is better with you in it"],
-    ["", "Please have a good day. If your day is not good, maybe tomorrow will be a good day"],
-    ["", "Rules are made to be broken and you are made to be happy"],
-    ["", "Study while others are sleeping; work while others are loafing; prepare while others are playing; and dream while others are wishing"],
-    ["", "You tend to underrate yourself, sometimes you have to pat yourself on the back"],
-    ["", "When there is an elephant in the room, introduce him"],
-    ["Antoine de Saint-Exupéry, Le Petit Prince", "Fais de ta vie un rêve et d'un rêve une réalité"],
-    ["Antoine de Saint-Exupéry, Le Petit Prince", "People where you live grow five thousand roses in one garden... yet they don't find what they're looking for..."],
-    ["Bocchi The Rock!", "You see, Nijika, the thing about dreams... is that when times get tough... they can be the light to guide your way"],
-    ["", "Cats don't have to assure people that they are cats"],
-    ["Edna Mode", "I never look back darling, it distracts from the now."],
-    ["Girls last tour, Endless Journey", "This endless road is not a mistake. It's to the future."],
-    ["Harumaki Gohan, Reunion", "Let's make a promise to stay kids forever\nIf nobody gets hurt, stay as how our memoriees are"],
-    ["Jason Gray, Sparrows", "You can't add a single day by worrying\nYou'll worry your life away"],
-    ["kz and 八王子P and 栗山夕璃, トリコロール (Tricolor) (Tackmyn Y. translation)", "Whatever future lies ahead, you will be all right.\nYou'll illuminate it with the light you're yet to know."],
-    ["KZ-Livetune, ジュビリ", "We searched so long for perfect sound, in midst of crying, we're always laughing, and that's what's pushing us along."],
-    ["KZlivetune, Far Away (bambooXZX's translation)", "Maybe I'll come to love the unknown tomorrow"],
-    ["*Luna, Adventure Log", "I don't mind if it gets dirty because it looks better when it's worn out"],
-    ["*Luna, Check This Out", "No matter what anyone said, I want you to notice it\nCheck this out!"],
-    ["*Luna, バッドエンドの映画は嫌いなんだ (I hate bad end movies)", "Our \"best\" is still high in the sky,\nbut we can already see our \"worst\" below"],
-    ["*Luna, バッドエンドの映画は嫌いなんだ (I hate bad end movies)", "Those bad end movie\nI hate them!\nAnd I don't want to make myself the star of one"],
-    ["*Luna, 夏が僕らを呼ぶのなら (If Summer is calling us) ", "If there aren't enough promises, let's make them again and again"],
-    ["*Luna, 夏が僕らを呼ぶのなら (If Summer is calling us) ", "We can surely become stronger, we can look up, and we can walk again"],
-    ["*Luna, メタフィクション (Metafiction)", "I see, it's boring if you get to the last boss in a straight line."],
-    ["*Luna, 僕らの夏はまた巡って (Our Summer Will Come Again)", "There were a lot of bad times, weren't there?\nBut whenever I look back on it, those days seemed so beautiful"],
-    ["*Luna, 僕らの夏はまた巡って (Our Summer Will Come Again)", "What's wrong with crying out loud\nWhenever you fall, get scraped, or covered in mud?"],
-    ["*Luna, 僕らの夏はまた巡って (Our Summer Will Come Again)", "Was getting dirty and hurt that scary? "],
-    ["*Luna, 10年後の私になら / Only to me in 10 years (Kohana Lam's translation)", "It is not your fault that you cannot be strong."],
-    ["*Luna, 10年後の私になら / Only to me in 10 years (Kohana Lam's translation)", "You are still sneaky, aren't you?\nYou can't wait to escape"],
-    ["*Luna, 10年後の私になら / Only to me in 10 years (Kohana Lam's translation)", "You may not be able to run with these legs, but you are the one who chose trying to walk somehow"],
-    ["*Luna, あの夏のいつかは (Someday in that Summer)", "Failure is not scary at all, what's scary is to not leave anything behind"],
-    ["Mili, YUBIKIRI-GENMAN", "It's okay if you take a break\nFor I will never stop"],
-    ["Nanatsukaze, Harumeku", "I'd like to live like a cat someday\nHuman desires are foolish"],
-    ["OMORI, BASIL", "The photos in our album... They're not just photo. They're real memories."],
-    ["OMORI, HERO", "Life gets better. You just have to stay positive."],
-    ["OMORI, MEWO", "Meow? (Not much to do around here, is there?)"],
-    ["OMORI, MEWO", "Meow? (Waiting for something to happen?)"],
-    ["Orangestar, Daydream Skytrain (Translyrics by rachie)", "But if we just focus on things that annoy us, we might miss the stars"],
-    ["Orangestar, Daydream Skytrain (Translyrics by rachie)", "Hey, don't just wait — make it happen instead"],
-    ["Orangestar, Daydream Skytrain (Translyrics by rachie)", "If we're gonna waste the summer, might as well we make them remember just what we can be"],
-    ["PIKASONIC, ColorCode-F", "If your dream is stained with someone else's color\nLet's go back to the beginning color."],
-    ["PIKASONIC, First Magnitude Star", "I want to sing this song to you, who can't shine in a simple world"],
-    ["Qeiru, フィクション (Fiction)", "Let's rest a little longer, until the sadness melts away"],
-    ["Qeiru, フィクション (Fiction)", "...even if that's the case, my imagination, my adventure, is not a lie"],
-    ["Qeiru, 世界線第二ターミナル (World Line Terminal 2)", "Hating the hopeless present, I throw it away, saying I don't know anymore"],
-    ["Yoshida, 夏空のSELFISH (Selfish in the summer sky)", "A small happiness is fine\ndon't need anything else"],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-    // ["", ""],
-];
+const Quotes = 
+[
+    [
+        "",
+        "Be kind, I don't care why"
+    ],
+    [
+        "",
+        "Be the change you want to see in the world"
+    ],
+    [
+        "",
+        "C'est juste un arrêt, pas le terminus."
+    ],
+    [
+        "",
+        "Cats don't have to assure people that they are cats"
+    ],
+    [
+        "",
+        "Certains jours, ils sont foutus et ne peuvent pas être défoncés"
+    ],
+    [
+        "",
+        "Devil works hard for his paycheck but you work harder."
+    ],
+    [
+        "",
+        "Don't lose sight of the moment that you're given"
+    ],
+    [
+        "",
+        "Even if I am a free wind, I'll cherish those who love me"
+    ],
+    [
+        "",
+        "Event a broken clock is right twice a day"
+    ],
+    [
+        "",
+        "Everybody is an astronaut because the Earth, after all, is in space"
+    ],
+    [
+        "",
+        "Everybody want to know what I will do if I didn't win.\nThat is not an option, so I don't think about it."
+    ],
+    [
+        "",
+        "I have to do it the classic way, which is... just find out..."
+    ],
+    [
+        "",
+        "I might not be normal, but I am happy"
+    ],
+    [
+        "",
+        "I will be the one who light the flame"
+    ],
+    [
+        "",
+        "Il y a toujours des choses qui sont plus importantes que d'autres"
+    ],
+    [
+        "",
+        "It started in the most unsuspecting location but again, that is where all the fairy tales happen"
+    ],
+    [
+        "",
+        "Je dois le faire de manière classique, c'est-à-dire... simplement découvrir..."
+    ],
+    [
+        "",
+        "Je me plains seulement de ton malheur, tu n'ne qu'un vulgaire carlin !"
+    ],
+    [
+        "",
+        "La vie est comme des toilettes, parfois il faut juste laisser tomber la merde et chasser la négativité"
+    ],
+    [
+        "",
+        "La vie n'est pas la fête qu'on imagine mais puisque nous sommes là, dansons!"
+    ],
+    [
+        "",
+        "Le seul endroit où le succès vient avant le travail est dans le dictionnaire"
+    ],
+    [
+        "",
+        "Les gens ne peuvent pas vous rendre fou si vous ne leur donnez pas les clés"
+    ],
+    [
+        "",
+        "Life is like a toilet, sometimes you just gotta let go of the crap and flush away the negativity"
+    ],
+    [
+        "",
+        "Life isn't the party we imagined but since we are here, let's dance!"
+    ],
+    [
+        "",
+        "People can't drive you crazy if you don't give them the keys"
+    ],
+    [
+        "",
+        "Please have a good day. If your day is not good, maybe tomorrow will be a good day"
+    ],
+    [
+        "",
+        "Rules are made to be broken and you are made to be happy"
+    ],
+    [
+        "",
+        "Some day are fucked and cannot be unfucked"
+    ],
+    [
+        "",
+        "Study while others are sleeping; work while others are loafing; prepare while others are playing; and dream while others are wishing"
+    ],
+    [
+        "",
+        "The horror persists, but so do I"
+    ],
+    [
+        "",
+        "The only place success comes before work is in the dictionary"
+    ],
+    [
+        "",
+        "The world is better with you in it"
+    ],
+    [
+        "",
+        "There are alyways some things that are more important than others"
+    ],
+    [
+        "",
+        "Tout a commencé dans l'endroit le plus insoupçonné, mais encore un fois, c'est là que se déroulent tous les contes de fées"
+    ],
+    [
+        "",
+        "Tout le monde veut savoir ce que je ferais si je ne gagnais pas.\nCe n'est une option, doc je n'y pense pas."
+    ],
+    [
+        "",
+        "When there is an elephant in the room, introduce him"
+    ],
+    [
+        "",
+        "You tend to underrate yourself, sometimes you have to pat yourself on the back"
+    ],
+    [
+        "*Luna, 10年後の私になら / Only to me in 10 years (Kohana Lam's translation)",
+        "It is not your fault that you cannot be strong."
+    ],
+    [
+        "*Luna, 10年後の私になら / Only to me in 10 years (Kohana Lam's translation)",
+        "You are still sneaky, aren't you?\nYou can't wait to escape"
+    ],
+    [
+        "*Luna, 10年後の私になら / Only to me in 10 years (Kohana Lam's translation)",
+        "You may not be able to run with these legs, but you are the one who chose trying to walk somehow"
+    ],
+    [
+        "*Luna, Adventure Log",
+        "Cela ne me dérange pas s'il se salit, car il est plus beau lorsqu'il est usé"
+    ],
+    [
+        "*Luna, Adventure Log",
+        "I don't mind if it gets dirty because it looks better when it's worn out"
+    ],
+    [
+        "*Luna, Check This Out",
+        "No matter what anyone said, I want you to notice it\nCheck this out!"
+    ],
+    [
+        "*Luna, Check This Out",
+        "Tu m'es appris que c'était bien d'être fou"
+    ],
+    [
+        "*Luna, Check This Out",
+        "You taught me that it's nice to be crazy"
+    ],
+    [
+        "*Luna, あの夏のいつかは (Someday in that Summer)",
+        "Failure is not scary at all, what's scary is to not leave anything behind"
+    ],
+    [
+        "*Luna, バッドエンドの映画は嫌いなんだ (I hate bad end movies)",
+        "Our \"best\" is still high in the sky,\nbut we can already see our \"worst\" below"
+    ],
+    [
+        "*Luna, バッドエンドの映画は嫌いなんだ (I hate bad end movies)",
+        "Those bad end movie\nI hate them!\nAnd I don't want to make myself the star of one"
+    ],
+    [
+        "*Luna, メタフィクション (Metafiction)",
+        "I see, it's boring if you get to the last boss in a straight line."
+    ],
+    [
+        "*Luna, 僕らの夏はまた巡って (Our Summer Will Come Again)",
+        "There were a lot of bad times, weren't there?\nBut whenever I look back on it, those days seemed so beautiful"
+    ],
+    [
+        "*Luna, 僕らの夏はまた巡って (Our Summer Will Come Again)",
+        "Was getting dirty and hurt that scary? "
+    ],
+    [
+        "*Luna, 僕らの夏はまた巡って (Our Summer Will Come Again)",
+        "What's wrong with crying out loud\nWhenever you fall, get scraped, or covered in mud?"
+    ],
+    [
+        "*Luna, 夏が僕らを呼ぶのなら (If Summer is calling us) ",
+        "If there aren't enough promises, let's make them again and again"
+    ],
+    [
+        "*Luna, 夏が僕らを呼ぶのなら (If Summer is calling us) ",
+        "We can surely become stronger, we can look up, and we can walk again"
+    ],
+    [
+        "Antoine de Saint-Exupéry, Le Petit Prince",
+        "Fais de ta vie un rêve et d'un rêve une réalité"
+    ],
+    [
+        "Antoine de Saint-Exupéry, Le Petit Prince",
+        "People where you live grow five thousand roses in one garden... yet they don't find what they're looking for..."
+    ],
+    [
+        "Bocchi The Rock!",
+        "You see, Nijika, the thing about dreams... is that when times get tough... they can be the light to guide your way"
+    ],
+    [
+        "Ciel Phantomhive",
+        "Only complaining on your misfortune, youère nothing but a common pug!"
+    ],
+    [
+        "Edna Mode",
+        "I never look back darling, it distracts from the now."
+    ],
+    [
+        "Girls last tour, Endless Journey",
+        "This endless road is not a mistake. It's to the future."
+    ],
+    [
+        "Harumaki Gohan, Reunion",
+        "Let's make a promise to stay kids forever\nIf nobody gets hurt, stay as how our memoriees are"
+    ],
+    [
+        "Jason Gray, Sparrows",
+        "You can't add a single day by worrying\nYou'll worry your life away"
+    ],
+    [
+        "kz and 八王子P and 栗山夕璃, トリコロール (Tricolor) (Tackmyn Y. translation)",
+        "Whatever future lies ahead, you will be all right.\nYou'll illuminate it with the light you're yet to know."
+    ],
+    [
+        "KZ-Livetune, ジュビリ",
+        "We searched so long for perfect sound, in midst of crying, we're always laughing, and that's what's pushing us along."
+    ],
+    [
+        "KZlivetune, Far Away (bambooXZX's translation)",
+        "Maybe I'll come to love the unknown tomorrow"
+    ],
+    [
+        "Les Cowboys Fringants, Les étoiles filantes",
+        "Cause you know, to look too far ahead is no better than looking back"
+    ],
+    [
+        "Les Cowboys Fringants, Les étoiles filantes",
+        "Parce que tsé, voir trop loin\nC'past mieux que de r'garder en arrière"
+    ],
+    [
+        "MgTyle",
+        "Chill, have some milk"
+    ],
+    [
+        "Mili, YUBIKIRI-GENMAN",
+        "It's okay if you take a break\nFor I will never stop"
+    ],
+    [
+        "Nanatsukaze, Harumeku",
+        "I'd like to live like a cat someday\nHuman desires are foolish"
+    ],
+    [
+        "OMORI, BASIL",
+        "The photos in our album... They're not just photo. They're real memories."
+    ],
+    [
+        "OMORI, HERO",
+        "Life gets better. You just have to stay positive."
+    ],
+    [
+        "OMORI, MEWO",
+        "Meow? (Not much to do around here, is there?)"
+    ],
+    [
+        "OMORI, MEWO",
+        "Meow? (Waiting for something to happen?)"
+    ],
+    [
+        "Orangestar, Daydream Skytrain (Translyrics by rachie)",
+        "But if we just focus on things that annoy us, we might miss the stars"
+    ],
+    [
+        "Orangestar, Daydream Skytrain (Translyrics by rachie)",
+        "Hey, don't just wait — make it happen instead"
+    ],
+    [
+        "Orangestar, Daydream Skytrain (Translyrics by rachie)",
+        "If we're gonna waste the summer, might as well we make them remember just what we can be"
+    ],
+    [
+        "PIKASONIC, ColorCode-F",
+        "If your dream is stained with someone else's color\nLet's go back to the beginning color."
+    ],
+    [
+        "PIKASONIC, First Magnitude Star",
+        "I want to sing this song to you, who can't shine in a simple world"
+    ],
+    [
+        "Qeiru, フィクション (Fiction)",
+        "...even if that's the case, my imagination, my adventure, is not a lie"
+    ],
+    [
+        "Qeiru, フィクション (Fiction)",
+        "Let's rest a little longer, until the sadness melts away"
+    ],
+    [
+        "Qeiru, 世界線第二ターミナル (World Line Terminal 2)",
+        "Hating the hopeless present, I throw it away, saying I don't know anymore"
+    ],
+    [
+        "Stardust Telepath",
+        "I don't think making other lose is the only condition of winning"
+    ],
+    [
+        "Stephen Hawking",
+        "Alors n'oubliez pas de regarder les étoiles et non vos pieds"
+    ],
+    [
+        "Stephen Hawking",
+        "So remember to look up at the stars and not down to your feet"
+    ],
+    [
+        "Yoshida, 夏空のSELFISH (Selfish in the summer sky)",
+        "A small happiness is fine\ndon't need anything else"
+    ]
+]
